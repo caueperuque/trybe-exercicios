4b436518fb021a8e8168e08c9161d64ee0c6f216
caue peruque <caue19052003@gmail.com>
2023-01-23 16:08:39 -0300

    exercicio fixar 04 resolvido

--- modulo-01_fundamentos/secao-03_JavaScript_primeiros_passos/scripts.js
+++ modulo-01_fundamentos/secao-03_JavaScript_primeiros_passos/scripts.js
@@ -1,15 +1,27 @@
 //ETAPA 01
-const test = 90;
+const currentHour = 23;
+let message = null;
 
 //ETAPA 02
-if (test >= 80) {
-    console.log('Parabéns, você faz parte do grupo das pessoas aprovadas!');
+if (currentHour >= 22) {
+    massage = 'Não deveríamos comer nada, é hora de dormir';
 }
 
-else if (test >= 60 && test < 80) {
-    console.log('Você está na nossa lista de espera');
+else if(currentHour >= 18 && currentHour < 22) {
+    massage = 'Rango da noite, vamos jantar :D';
+}
+
+else if(currentHour >= 14 && currentHour < 18) {
+    massage = 'Vamos fazer um bolo pro café da tarde?';
+}
+
+else if(currentHour >= 11 && currentHour < 14) {
+    massage = 'Hora do almoço!!!';
 }
 
 else{
-   console.log('Infelizmente, você reprovou.') 
+    massage = 'Hmmm, cheiro de café recém-passado';
 }
+
+//ETAPA 03
+console.log(massage);
