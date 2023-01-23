d4eb0903c172473fbbf18eb2db4de0ed9bad073e
caue peruque <caue19052003@gmail.com>
2023-01-23 16:23:30 -0300

    exercício fixar 06 resolvido

--- modulo-01_fundamentos/secao-03_JavaScript_primeiros_passos/scripts.js
+++ modulo-01_fundamentos/secao-03_JavaScript_primeiros_passos/scripts.js
@@ -1,16 +1,15 @@
-//ETAPA 01
-let weekDay = 'julho'
+let statusTest = 'aprovada'
 
-//ETAPA 02
-if (weekDay === 'segunda-feira' || weekDay === 'terça-feira' || weekDay === 'quarta-feira' || weekDay === 'quinta-feira' || weekDay === 'sexta-feira') {
-    console.log('Oba, mais um dia de aprendizado na Trybe >:D');
-}
-
-else if (weekDay === 'sábado' || weekDay === 'sabado' || weekDay === 'domingo'){
-    console.log('FINALMENTE, descanso merecido UwU');
-}
-
-//ACRESCENTEI UM NOVO ALGORITMO
-else{
-    console.log(weekDay + ', não é um dia da semana!');
+switch(statusTest){
+    case 'aprovada':
+        console.log('Parapéns, vocÊ foi aprovada(o)');
+        break;
+    case 'lista':
+        console.log('Você está na nossa lista de espera');
+        break;
+    case 'reprovada':
+        console.log('Você foi reprovada(o)');
+        break;
+    default:
+        console.log('Informação incorreta');
 }
