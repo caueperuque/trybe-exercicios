0ec5e2b4242d2a29ad1d2692b7b2582df5ef9638
caue peruque <caue19052003@gmail.com>
2023-01-31 16:05:10 -0300

    realizando exercicios de aula

---
+++ modulo-01_fundamentos/secao-04-DOM-Js/script.js
@@ -0,0 +1,4 @@
+document.getElementById("page-title").innerText = "Interestelar";
+document.getElementsByClassName("paragraph")[0].innerText = "Modificando primeiro parágrafo com getElementsByClassName";
+document.getElementById("subtitle").innerText = "Melhor filme de todos";
+document.getElementsByTagName("h4")[0].innerText = "Melhor filme de todos 2.0";
